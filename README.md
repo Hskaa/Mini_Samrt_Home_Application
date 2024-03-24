82dc091ca36b53369be0e18190c198ca3931aac8
Hskaa <hossammedhat81@yahoo.com>
2024-03-24 17:15:43 +0200

    Mini Smart Home

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Mini_Samrt_Home_Application
- Application has small orders to mini smart home
+# Mini_SmartHome
+ Application of smart home that has small orders 
---
+++ project_cfg.h
@@ -0,0 +1,17 @@
+/*****************************************************/
+/* Author      : alhosin & Hossam                    */
+/*                                                   */
+/*****************************************************/
+
+#ifndef PROJECT_CFG_H_
+#define PROJECT_CFG_H_
+
+#define PRO_PORT DIO_PORTC
+
+#define RedLedPin DIO_PIN0
+#define GreenLedPin DIO_PIN1
+#define LightPin DIO_PIN2
+#define BuzzerPin DIO_PIN3
+#define FanPin DIO_PIN4
+
+#endif
